976633bfffd62cf375a7f995e5aa11f4ab7b4650
Mineli <monte.nemi@gmail.com>
2020-03-01 22:28:35 +0100

    updated

--- index.php
+++ index.php
@@ -4,18 +4,18 @@ include "includes/header.php";
 ?>
 
 <div class="container">
-    <div class="jumbotron mt-5">
-        <h1 class="display-4">Tester les multiplications</h1>
-        <p class="lead">Ici, vous pouvez afficher les differentes tables de multiplications et verifier votre connaissance de cette operation</p>
-        <hr class="my-4">
-        <p>Si vous voulez afficher les differentes tables de multiplications, clickez sur le button en-dessous.</p>
-        <p class="lead">
-            <a class="btn btn-primary btn-lg" href="show-tables.php" role="button">Clicker ici</a>
-        </p>
-        <hr class="my-4">
-        <p>Si vous voulez tester les multiplications, clickez sur le button en-dessous.</p>
-        <p class="lead">
-            <a class="btn btn-primary btn-lg" href="test.php" role="button">Clicker ici</a>
-        </p>
-    </div>
+  <div class="jumbotron mt-5">
+    <h1 class="display-4">Tester les multiplications</h1>
+    <p class="lead">Ici, vous pouvez afficher les differentes tables de multiplications et verifier votre connaissance de cette operation</p>
+    <hr class="my-4">
+    <p>Si vous voulez afficher les differentes tables de multiplications, clickez sur le button en-dessous.</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="show-tables.php" role="button">Clicker ici</a>
+    </p>
+    <hr class="my-4">
+    <p>Si vous voulez tester les multiplications, clickez sur le button en-dessous.</p>
+    <p class="lead">
+      <a class="btn btn-primary btn-lg" href="test.php" role="button">Clicker ici</a>
+    </p>
+  </div>
 </div>
--- show-tables.php
+++ show-tables.php
@@ -5,36 +5,69 @@ include "includes/footer.php";
 ?>
 
 <div class="container">
-    <h1>Selectionez les tables de multiplication</h1>
+  <h1>Selectionez les tables de multiplication</h1>
 
-    <form id="test-form" class="form-inline" action="" method="GET">
-        <?php
-        for($i = 1; $i <= 10; $i++) {
-            echo '<div class="form-check form-check-inline">
-                    <input class="form-check-input" type="checkbox" name="num[]" value="' . $i . '">' . $i . '</div>';
-        }
-        ?>
-        <div class="form-check form-check-inline">
-            <input class="form-check-input btn bg-info" type="submit" name="submit" value="Afficher">
-        </div>
-    </form>
-
-<div id="serverResponse"></div>
-
-<script type="text/javascript">
-    let formElement = document.querySelector("form");
-    const hr = new XMLHttpRequest();
-
-    hr.onload = function() {
-        const serverResponse = document.getElementById("serverResponse");
-        serverResponse.innerHTML = hr.responseText;
-    };
-
-    hr.open("GET", "submitForm.php");
-    hr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
-    hr.send(new FormData(formElement));
-</script>
-
+  <!-- <form id="test-form" class="form-inline" action="" method="GET"> -->
+  <?php
+  // for ($i = 1; $i <= 10; $i++) {
+  // echo '<div class="form-check form-check-inline">
+  // <input class="form-check-input" type="checkbox" name="num[]" value="' . $i . '">' . $i . '</div>';
+  // }
+  // 
+  ?>
+  <!-- <div class="form-check form-check-inline">
+<input class="form-check-input btn bg-info" type="submit" name="submit" value="Afficher">
 </div>
+</form> -->
 
+  <form id="test-form" class="form-inline" action="" method="GET">
+    <?php
+    for ($i = 1; $i <= 10; $i++) {
+      echo '<div class="form-check form-check-inline">
+<input class="form-check-input" type="checkbox" name="' . $i . 'fois" value="' . $i . '">' . $i .
+        '</div>';
+    }
+    ?>
+    <div class="form-check form-check-inline">
+      <input class="form-check-input btn bg-info" type="submit" name="submit" value="Afficher">
+    </div>
+  </form>
+
+  <?php
+  if (isset($_GET["submit"])) {
+    $arr = [];
+
+    for ($i = 0; $i < 10; $i++) {
+      $arr[$i] = (isset($_GET[($i + 1) . 'fois']));
+    }
+
+    if (array_sum($arr) === 0) { // boolean values are casted as 1 for true and 0 for false
+      echo "<h2>Vous devez cocher au moins une case</h2>";
+    } else {
+      echo "<h2>Vous avez selectionné les tables suivantes:</h2>";
+      echo "Number of the checked boxes: " . array_sum($arr) . '<br>';
+    }
+  ?>
+
+    <div class="tables">
+      <?php
+      foreach ($arr as $key => $value) {
+        if ($value) {
+      ?>
+          <div class="table">
+            <?php
+            for ($k = 1; $k <= 10; $k++) {
+              echo $key + 1 . ' * ' . $k . ' = ' . ($key + 1) * $k . '<br>';
+            }
+            ?>
+          </div>
+      <?php
+        }
+      }
+      ?>
+    </div>
+
+  <?php
+  }
+  ?>
+</div>
